797bf5ffd82a3640eed6a2ce7a913c13bde02546
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2022-11-29 19:14:10 +0000

    saved

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import react,  {useState}  from 'react';
+import Navbar from "./Component/Navigation";
+import Counter from './Component/Counter';
+
+function App() {
+  const [searchString, setSearchString] = useState('hi');
+  return (
+    <div className="App">
+      <Navbar />
+      <Counter /> 
+    </div>
+  );
+}
+
+export default App;
